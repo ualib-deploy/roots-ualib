c38934eda29cce263b24f140fde57768dc695e5e
bkrylov <bvkrylov@ua.edu>
2015-03-31 14:24:15 -0500

    Test front page changes and sidebar kill

--- base.php
+++ base.php
@@ -20,12 +20,18 @@
         <main class="main" role="main">
           <?php include roots_template_path(); ?>
         </main><!-- /.main -->
-        <?php if (roots_display_sidebar()) : ?>
-          <aside class="sidebar" role="complementary">
-            <?php include roots_sidebar_path(); ?>
+        <?php
+            if (roots_display_sidebar()) :
+        ?>
+<!--          <aside class="sidebar" role="complementary">
+            <?php
+//                include roots_sidebar_path();
+            ?>
           </aside><!-- /.sidebar -->
-        <?php endif; ?>
-      </div><!-- /.content -->
+        <?php
+            endif;
+        ?>
+-->      </div><!-- /.content -->
     </div><!-- /.wrap -->
   </div>
 
