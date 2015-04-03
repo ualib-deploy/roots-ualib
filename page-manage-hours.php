5ca3c97fd065068b2c45196ea76c9c645d371e0f
bvkrylov <bvkrylov@ua.edu>
2015-04-03 12:51:15 -0500

    Manage OneSearch bug fix2

---
+++ page-manage-hours.php
@@ -0,0 +1,20 @@
+<?php
+
+if( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/*
+* Template Name: Hours Management
+* Description: Hours Management template.
+ */
+?>
+
+<div id="content">
+
+    <?php
+        include WEBAPPS_PATH . "libhours2/manage_hours_wp.php";
+	?>
+
+</div><!-- end of #content -->
+
---
+++ page-manage-onesearch.php
@@ -0,0 +1,20 @@
+<?php
+
+if( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/*
+* Template Name: Hours Management
+* Description: Hours Management template.
+ */
+?>
+
+<div id="content">
+
+    <?php
+        include WEBAPPS_PATH . "oneSearch/manage_wp.php";
+	?>
+
+</div><!-- end of #content -->
+
