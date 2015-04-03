19c917a0ef4fb968eb031e20bedc938ee232bb60
bvkrylov <bvkrylov@ua.edu>
2015-04-03 10:49:59 -0500

    Cleared trash from theme functions file

--- functions-ualib.php
+++ functions-ualib.php
@@ -2,15 +2,9 @@
 
 define( 'WEBAPPS_PATH', '/srv/web/www/webapps/' );
 
-function ualib_getDomain()
-{
-  return "//wwwdev2.lib.ua.edu/";
-}
-
 function ualib_child_startSession() {
-  if(!session_id()) {
+  if(!session_id())
     session_start();
-  }
 }
 
 function ualib_child_endSession() {
@@ -19,88 +13,11 @@ function ualib_child_endSession() {
 
 function ualib_scripts() {
   if ( is_page('hours') )
-  {
     wp_enqueue_script(
       'GoogleMapAPI',
       '//maps.googleapis.com/maps/api/js?key=AIzaSyDCIMNYW9I2NfZfh83u-NRPUgHlUG51Hfc'
     );
-    wp_enqueue_script(
-      'ualib-child-scriptHours',
-      ualib_getDomain() . 'libhours2/js/app.js',
-      array( 'angular' ),
-      true
-    );
-  }
-  if ( is_page('music-library-search') )
-    wp_enqueue_script(
-      'ualib-child-scriptMusicSearch',
-      ualib_getDomain() . 'musicsearch/js/app.js',
-      array( 'angular' ),
-      true
-    );
-  if ( is_page('staff-directory') )
-    wp_enqueue_script(
-      'ualib-child-scriptStaffDirectory',
-      ualib_getDomain() . 'staffDir/js/app.js',
-      array( 'angular' ),
-      true
-    );
-  if ( is_page('databases') )
-    wp_enqueue_script(
-      'ualib-child-scriptDatabases',
-      ualib_getDomain() . 'databases/js/app.js',
-      array( 'angular' ),
-      true
-    );
-  if ( is_page('user-groups-admin') )
-    wp_enqueue_script(
-      'ualib-child-scriptUserGroups',
-      ualib_getDomain() . 'userGroupsAdmin/js/app.js',
-      array( 'angular' ),
-      true
-    );
-  if ( is_page('site-feedback') )
-    wp_enqueue_script(
-      'ualib-child-siteFeedback',
-      ualib_getDomain() . 'siteSurvey/js/app.js',
-      array( 'angular' ),
-      true
-    );
-
 }
-
-function ualib_demo_scripts(){
-
-  if ( is_page('search') )
-  {
-    wp_enqueue_script('ng', 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0/angular.min.js', array(), false, true);
-    wp_enqueue_script('ngAnimate', 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0/angular-animate.min.js', array('ng'), false, true);
-    wp_enqueue_script('ngSanitize', 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0/angular-sanitize.js', array('ng'), false, true);
-    wp_enqueue_script('angular-filter', 'https://cdnjs.cloudflare.com/ajax/libs/angular-filter/0.5.1/angular-filter.js', array('ng'), false, true);
-    wp_enqueue_script('ui-bootstrap-12', 'https://rawgit.com/angular-ui/bootstrap/gh-pages/ui-bootstrap-tpls-0.12.0.min.js', array('ng'), false, true);
-    wp_enqueue_script(
-      'oneSearchTpl',
-      ualib_getDomain() . 'oneSearch/will-test/onesearch-templates.js',
-      array(),
-      false,
-      true
-    );
-    wp_enqueue_script(
-      'oneSearch',
-      ualib_getDomain() . 'oneSearch/will-test/onesearch.js',
-      array(),
-      false,
-      true
-    );
-    wp_enqueue_style(
-      'oneSearchStyles',
-      ualib_getDomain() . 'oneSearch/will-test/onesearch.css',
-      false,
-      null
-    );
-  }
-}
-
 
 remove_filter('the_content', 'wpautop');
 remove_filter('the_excerpt', 'wpautop');
@@ -110,4 +27,4 @@ add_action('wp_logout', 'ualib_child_endSession');
 add_action('wp_login', 'ualib_child_endSession');
 
 add_action( 'wp_enqueue_scripts', 'ualib_scripts' );
-//add_action( 'wp_enqueue_scripts', 'ualib_demo_scripts' );
+
