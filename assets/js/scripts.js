a25a857c61f4629f3c2330fa5682547d016fb249
bvkrylov <bvkrylov@ua.edu>
2015-05-08 16:30:32 -0500

    Manage software app update 2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -39444,11 +39444,11 @@ angular.module("manageSoftware/manageSoftware.tpl.html", []).run(["$templateCach
     "                <div class=\"col-md-12\">\n" +
     "                    <div class=\"col-md-6 form-group\">\n" +
     "                        <label for=\"{{sw.sid}}_descr\">Description</label>\n" +
-    "                        <textarea class=\"form-control\" rows=\"2\" id=\"{{sw.sid}}_descr\" ng-model=\"sw.description\" ></textarea>\n" +
+    "                        <textarea class=\"form-control\" rows=\"3\" id=\"{{sw.sid}}_descr\" ng-model=\"sw.description\" ></textarea>\n" +
     "                    </div>\n" +
     "                    <div class=\"col-md-6 form-group\">\n" +
     "                        <label for=\"{{sw.sid}}_det\">Details (Get it)</label>\n" +
-    "                        <textarea class=\"form-control\" rows=\"2\" id=\"{{sw.sid}}_det\" ng-model=\"sw.details\" ></textarea>\n" +
+    "                        <textarea class=\"form-control\" rows=\"3\" id=\"{{sw.sid}}_det\" ng-model=\"sw.details\" ></textarea>\n" +
     "                    </div>\n" +
     "                </div>\n" +
     "                <div class=\"col-md-12\">\n" +
@@ -39594,11 +39594,13 @@ angular.module("manageSoftware/manageSoftware.tpl.html", []).run(["$templateCach
     "        <div class=\"col-md-12\">\n" +
     "            <div class=\"col-md-6 form-group\">\n" +
     "                <label for=\"descr\">Description</label>\n" +
-    "                <textarea class=\"form-control\" rows=\"2\" id=\"descr\" ng-model=\"newSW.description\" ></textarea>\n" +
+    "                <textarea class=\"form-control\" rows=\"3\" id=\"descr\" ng-model=\"newSW.description\" ></textarea>\n" +
     "            </div>\n" +
     "            <div class=\"col-md-6 form-group\">\n" +
     "                <label for=\"det\">Details (Get it)</label>\n" +
-    "                <textarea class=\"form-control\" rows=\"2\" id=\"det\" ng-model=\"newSW.details\" ></textarea>\n" +
+    "                <textarea class=\"form-control\" rows=\"3\" id=\"det\" ng-model=\"newSW.details\" >\n" +
+    "\n" +
+    "                </textarea>\n" +
     "            </div>\n" +
     "        </div>\n" +
     "        <div class=\"col-md-12\">\n" +
@@ -41139,6 +41141,14 @@ angular.module('manage.manageSoftware', ['ngFileUpload'])
             $scope.newSW.newVer = {};
             $scope.newSW.newVer.selOS = $scope.os[0];
             $scope.newSW.newLink = {};
+            $scope.newSW.newLink.title = "LibGuide";
+            $scope.newSW.newLink.url = "http://guides.lib.ua.edu/";
+            $scope.newSW.details = '<ul><li><strong>At home:</strong> Students and faculty/staff:' +
+                '<a href="http://oit.ua.edu/oit/services/software-licensing/microsoft-office-for-students#eligibility">' +
+                'Microsoft Office for Personal Use</a></li><li>' +
+                '<strong>On campus:</strong> University-owned machines:' +
+                '<a href="http://oit.ua.edu/oit/services/software-licensing/microsoft-campus-agreement#eligibility">' +
+                'Microsoft Campus Agreement</a></li></ul>';
 
             $scope.currentPage = 1;
             $scope.maxPageSize = 10;
