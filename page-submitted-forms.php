f4d08f9412224b388bd2ae22a04b476650cf2de0
bvkrylov <bvkrylov@ua.edu>
2015-05-13 11:18:50 -0500

    Manage news and exhibitions app added

---
+++ page-submitted-forms.php
@@ -0,0 +1,19 @@
+<?php
+
+if( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+/*
+* Template Name: Hours Management
+* Description: Hours Management template.
+ */
+?>
+
+<div id="content">
+
+    <?php
+    include WEBAPPS_PATH . "forms/manage_wp.php";
+    ?>
+
+</div><!-- end of #content -->
