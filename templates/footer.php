a2786a2ac6db0fb73b2eb496d49c56898c9fca64
dfquintel <denise.quintel@gmail.com>
2015-05-22 13:32:12 -0500

    Edited lists in header

--- templates/footer.php
+++ templates/footer.php
@@ -26,12 +26,11 @@
             <li><a href="<?php echo site_url(); ?>/libraries-and-collections/rodgers-science-and-engineering-library/">Rodgers Library for Science &amp; Engineering</a></li>
             <li><a href="<?php echo site_url(); ?>/libraries-and-collections/hoole-library/">W.S. Hoole Special Collections Library</a></li>
             <li><a href="<?php echo site_url(); ?>/libraries-and-collections/music-library/">Music Library</a></li>
-            <li><a href="<?php echo site_url(); ?>/services/sanford-media-center/">Sanford Media Center</a></li>
-            <li><a href="<?php echo site_url(); ?>/services/digital-humanities-center/">Alabama Digital Humanities Center</a></li>
             <li><a href="<?php echo site_url(); ?>/libraries-and-collections/campus-libraries/bounds-law-library/">Bounds Law Library</a></li>
             <li><a href="<?php echo site_url(); ?>/libraries-and-collections/campus-libraries/health-sciences-library/">Health Sciences Library</a></li>
             <li><a href="<?php echo site_url(); ?>/libraries-and-collections/campus-libraries/map-library-place-names-research-center/">Map Library &amp; Place Names Research Center</a></li>
             <li><a href="http://wrc.ua.edu/library.cfm">Summersell Library</a></li>
+            <li><s href="http://cis.ua.edu/undergraduates/resources/readingroom/">William E. Winter Reading Room</li>
           </ul>
         </div>
 
@@ -44,17 +43,19 @@
             <li><a href="https://wwwdev2.lib.ua.edu/collections/map-collections/">Map Collection</a></li>
             <li><a href="https://wwwdev2.lib.ua.edu/collections/rare-book-collection/">Rare Book Collection</a></li>
             <li><a href="https://wwwdev2.lib.ua.edu/collections/university-archives/">University Archives and Records Management</a></li>
-            <li><a href="https://wwwdev2.lib.ua.edu/collections/major-collections-summaries/">More collections...</a></li>
+            <li><a href="https://wwwdev2.lib.ua.edu/collections/major-collections-summaries/">More Collections...</a></li>
           </ul>
        </div>
 
         <div class="col-xs-12 col-sm-3">
+            <a class="service-card href="wwwdev2.lib.ua.edu/about-ua-libraries/social-media/">
           <ul class="nav nav-pills nav-justified social-icons">
-            <li><a href="http://www.twitter.com"><span class="fa fa-fw fa-2x fa-twitter"></span></a></li>
-            <li><a href="http://www.instagram.com"><span class="fa fa-fw fa-2x fa-instagram"></span></a></li>
-            <li><a href="http://www.facebook.com"><span class="fa fa-fw fa-2x fa-facebook"></span></a></li>
-            <li><a href="http://www.youtube.com/"><span class="fa fa-fw fa-2x fa-youtube"></span></a></li>
+            <li><span class="fa fa-fw fa-2x fa-twitter"></span></a></li>
+            <li><span class="fa fa-fw fa-2x fa-instagram"></span></a></li>
+            <li><span class="fa fa-fw fa-2x fa-facebook"></span></a></li>
+            <li><span class="fa fa-fw fa-2x fa-youtube"></span></a></li>
           </ul>
+            </a>
 
           <a href="http://www.ua.edu">
               <img src="<?php print get_template_directory_uri(); ?>/assets/img/ua-capstone-logo.png" class="img-responsive" style="margin: 15px 0;"/>
