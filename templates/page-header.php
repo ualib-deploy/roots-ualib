d1bbf53d5bbe1002f3a7af0caf6c92d4947c2c6e
Will Jones <the8bitsquid@gmail.com>
2015-06-11 17:53:09 -0500

    Hide page header with custom field settings

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,8 +1,6 @@
-<?php $hide_title = is_array(get_field( "hide_page_header_title" )) && get_field( "hide_page_header_title" )[0] === 'hide'; ?>
-<?php if(!is_front_page() && !$hide_title): ?>
+<?php if(!is_front_page() && !(is_array(get_field( "hide_page_header_title" )) && get_field( "hide_page_header_title" )[0] === 'hide')): ?>
 <div class="page-header">
     <h1>
-        <?php print_r(get_field( "hide_page_header_title" )); ?>
         <?php echo roots_title(); ?>
     </h1>
 </div>
