0d709e8883fb237011334cda009c951066c72186
Will Jones <the8bitsquid@gmail.com>
2015-06-15 13:48:08 -0500

    Analytics wwdev2 domin test

--- base.php
+++ base.php
@@ -41,7 +41,7 @@
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-2255842-23', 'auto');
+      ga('create', 'UA-2255842-24', 'auto');
       // ga('send', 'pageview'); //### Not needed. Angular module handles this.
   </script>
 </body>
