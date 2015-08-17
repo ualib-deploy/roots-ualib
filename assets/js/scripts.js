a320f005858edfaee37f9ca93b480e77ada89095
willjones <willjones@ua.edu>
2015-08-16 23:06:51 -0500

    Added styles for btn-default in jumbotron-header. New hours build

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -41303,7 +41303,7 @@ angular.module("lib-hours-today/lib-hours-today.tpl.html", []).run(["$templateCa
     "        <span class=\"label\" ng-class=\"today.status.css\">{{today.status.text}}</span>\n" +
     "    </h2>\n" +
     "    <div class=\"h3\">{{today.hours}}</div>\n" +
-    "    <a class=\"btn btn-default btn-xs\" ng-href=\"/#hours?library={{today.name}}\" title=\"All hours {{today.name}}\">All hours <span class=\"fa fa-chevron-right\"></span></a>\n" +
+    "    <a class=\"btn btn-default btn-xs\" ng-href=\"/#hours?library={{today.name}}\" title=\"All hours {{today.name}}\">See all hours <span class=\"fa fa-fw fa-chevron-right\"></span></a>\n" +
     "</div>\n" +
     "");
 }]);
