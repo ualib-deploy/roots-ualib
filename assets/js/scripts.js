0bdbb125d953ad9323065ace6822690919f6e947
bvkrylov <bvkrylov@ua.edu>
2015-08-19 17:36:18 -0500

    Manage staffdir: still trying to fix tineMCE one-way ng-model behavior

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44758,7 +44758,7 @@ angular.module("staffDirectory/staffDirectoryProfile.tpl.html", []).run(["$templ
     "            <code>\n" +
     "                &lt;h3&gt;, &lt;h4&gt;, &lt;a&gt;, &lt;img&gt;, &lt;p&gt;, &lt;strong&gt;, &lt;em&gt;, &lt;ul&gt;, &lt;li&gt;\n" +
     "            </code>)</label>\n" +
-    "        <textarea data-ui-tinymce data-ng-model=\"userProfile.person.profile\" rows=\"10\"\n" +
+    "        <textarea ui-tinymce ng-model=\"userProfile.person.profile\" rows=\"10\"\n" +
     "                  maxlength=\"64000\"></textarea>\n" +
     "    </div>\n" +
     "    <div class=\"col-md-12 text-center form-group\">\n" +
