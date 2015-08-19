4f8ff4695a01a6cbc19ff5d380fb9bf264dd174a
bvkrylov <bvkrylov@ua.edu>
2015-08-19 12:41:06 -0500

    Manage StaffDir: added profile management: test2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44725,25 +44725,28 @@ angular.module("staffDirectory/staffDirectoryProfile.tpl.html", []).run(["$templ
     "<div class=\"page-slice\">\n" +
     "    <div class=\"row\">\n" +
     "        <div class=\"col-md-4\">\n" +
-    "            <img class=\"staff-portrait thumbnail\" ng-src=\"{{person.photo}}\" ng-if=\"person.photo != null\" width=\"180\" height=\"225\">\n" +
+    "            <img class=\"staff-portrait thumbnail\" ng-src=\"{{userProfile.person.photo}}\" ng-if=\"userProfile.person.photo != null\"\n" +
+    "                 width=\"180\" height=\"225\">\n" +
     "            <img class=\"staff-portrait thumbnail\" ng-src=\"wp-content/themes/roots-ualib/assets/img/user-profile.png\"\n" +
-    "                 ng-if=\"person.photo == null\" width=\"180\" height=\"225\">\n" +
+    "                 ng-if=\"userProfile.person.photo == null\" width=\"180\" height=\"225\">\n" +
     "        </div>\n" +
     "        <div class=\"col-md-8\">\n" +
     "            <div class=\"row\">\n" +
     "                <div class=\"col-xs-12 col-sm-7 name-plate\">\n" +
     "                    <h3 class=\"name\">\n" +
-    "                        <small ng-if=\"person.rank\">{{person.rank}}</small>\n" +
-    "                        <span ng-bind-html=\"person.firstname\"></span> <span ng-bind-html=\"person.lastname\"></span>\n" +
+    "                        <small ng-if=\"userProfile.person.rank\">{{userProfile.person.rank}}</small>\n" +
+    "                        <span ng-bind-html=\"userProfile.person.firstname\"></span> <span ng-bind-html=\"userProfile.person.lastname\"></span>\n" +
     "                    </h3>\n" +
-    "                    <h4 class=\"title\"><span ng-bind-html=\"person.title\"></span></h4>\n" +
-    "                    <h5 class=\"hidden-xs\"><span ng-bind-html=\"person.department\"></span></h5>\n" +
+    "                    <h4 class=\"title\"><span ng-bind-html=\"userProfile.person.title\"></span></h4>\n" +
+    "                    <h5 class=\"hidden-xs\"><span ng-bind-html=\"userProfile.person.department\"></span></h5>\n" +
     "                </div>\n" +
     "                <div class=\"col-xs-12 col-sm-5\">\n" +
     "                    <ul class=\"fa-ul\">\n" +
-    "                        <li ng-if=\"person.phone\"><span class=\"fa fa-phone fa-li\"></span>{{person.phone}}</li>\n" +
-    "                        <li class=\"hidden-xs\" ng-if=\"person.fax\"><span class=\"fa fa-fax fa-li\"></span>{{person.fax}}</li>\n" +
-    "                        <li ng-if=\"person.email\"><span class=\"fa fa-envelope fa-li\"></span><a href=\"mailto:{{person.email}}\">{{person.email}}</a></li>\n" +
+    "                        <li ng-if=\"userProfile.person.phone\"><span class=\"fa fa-phone fa-li\"></span>{{userProfile.person.phone}}</li>\n" +
+    "                        <li class=\"hidden-xs\" ng-if=\"userProfile.person.fax\"><span class=\"fa fa-fax fa-li\"></span>{{userProfile.person.fax}}</li>\n" +
+    "                        <li ng-if=\"userProfile.person.email\"><span class=\"fa fa-envelope fa-li\"></span>\n" +
+    "                            <a href=\"mailto:{{userProfile.person.email}}\">{{userProfile.person.email}}</a>\n" +
+    "                        </li>\n" +
     "                    </ul>\n" +
     "                </div>\n" +
     "            </div>\n" +
@@ -44755,12 +44758,12 @@ angular.module("staffDirectory/staffDirectoryProfile.tpl.html", []).run(["$templ
     "    <div class=\"row\">\n" +
     "        <div class=\"col-md-12 form-group\">\n" +
     "            <label for=\"profile\">Profile Content</label>\n" +
-    "            <textarea data-ui-tinymce id=\"profile\" data-ng-model=\"person.profile\" rows=\"10\"\n" +
+    "            <textarea data-ui-tinymce id=\"profile\" data-ng-model=\"userProfile.person.profile\" rows=\"10\"\n" +
     "                      maxlength=\"64000\"></textarea>\n" +
     "        </div>\n" +
     "        <div class=\"col-md-12 text-center form-group\">\n" +
     "            <button type=\"submit\" class=\"btn btn-success\" ng-disabled=\"uploading\">Update Profile</button><br>\n" +
-    "            {{person.formResponse}}\n" +
+    "            {{userProfile.person.formResponse}}\n" +
     "        </div>\n" +
     "    </div>\n" +
     "</div>\n" +
@@ -48230,7 +48233,6 @@ angular.module('manage.staffDirectory', [])
     .directive('editStaffDirectoryProfile', [ function() {
         return {
             restrict: 'AC',
-            require: 'name',
             scope: {
                 login: '=name'
             },
