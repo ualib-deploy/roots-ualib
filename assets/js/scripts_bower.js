d0ecce6084a7bfb1c98c4330c5d7cba8f99b3db1
bvkrylov <bvkrylov@ua.edu>
2015-12-03 16:28:51 -0600

    Manage forms: upload files version 3

--- assets/js/scripts_bower.js
+++ assets/js/scripts_bower.js
@@ -14766,7 +14766,7 @@ angular.module('manage.submittedForms', ['ngFileUpload'])
                     fields: {
                         form: form
                     },
-                    file: form.selectedFiles,
+                    file: $scope.form.selectedFiles,
                     fileFormDataName: names
                 });
                 $scope.form.selectedFiles.upload.then(function(response) {
