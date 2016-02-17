17e51815204c4d97c33acedf0dd9c3aa7270a5db
bvkrylov <bvkrylov@ua.edu>
2016-02-17 15:11:41 -0600

    Changed WP admin bar links generating function

--- functions-ualib.php
+++ functions-ualib.php
@@ -21,96 +21,96 @@ function add_manage_admin_bar_link() {
     @include_once WEBAPPS_PATH . "userGroupsAdmin/functions.php";
     if (!defined('GROUP_ANY_WEBAPP'))
         return;
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_ANY_WEBAPP )) !== false) {
+    if (($userGroup = gGetUserGroupWP()) !== false) {
         $wp_admin_bar->add_menu(array(
             'id' => 'manage_link',
             'title' => __('<span class="ab-icon dashicons-before dashicons-feedback"></span> UA Lib WebApps'),
             'href' => __(site_url() . "/#/manage-user-groups")
         ));
-        $wp_admin_bar->add_menu( array(
+        $wp_admin_bar->add_menu(array(
             'id' => 'manage_profile_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-welcome-write-blog"></span> Edit Profile'),
-            'href' => __(site_url()."/#/manage-my-profile"),
+            'title' => __('<span class="ab-icon dashicons-before dashicons-welcome-write-blog"></span> Edit Profile'),
+            'href' => __(site_url() . "/#/manage-my-profile"),
             'parent' => 'manage_link',
         ));
+        if (($userGroup & GROUP_ONESEARCH) === GROUP_ONESEARCH) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_onesearch_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-search"></span> oneSearch'),
+                'href' => __(site_url() . "/#/manage-onesearch"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_ADMIN) === GROUP_ADMIN) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_users_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-lock"></span> Permissions'),
+                'href' => __(site_url() . "/#/manage-user-groups"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_HOURS) === GROUP_HOURS) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_hours_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-clock"></span> Library Hours'),
+                'href' => __(site_url() . "/#/manage-hours"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_STAFFDIR) === GROUP_STAFFDIR) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_staffdir_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-groups"></span> Staff Directory'),
+                'href' => __(site_url() . "/#/manage-staff-directory"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_DATABASES) === GROUP_DATABASES) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_databases_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-networking"></span> Databases'),
+                'href' => __(site_url() . "/#/manage-databases"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_SOFTWARE) === GROUP_SOFTWARE) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_software_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-analytics"></span> Software'),
+                'href' => __(site_url() . "/#/manage-software"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_FORMS) === GROUP_FORMS) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_forms_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-clipboard"></span> Forms'),
+                'href' => __(site_url() . "/#/manage-forms"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_NEWS) === GROUP_NEWS) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_news_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-calendar"></span> News &amp; Exhibits'),
+                'href' => __(site_url() . "/#/manage-news"),
+                'parent' => 'manage_link',
+            ));
+        }
+        if (($userGroup & GROUP_ALERTS) === GROUP_ALERTS) {
+            $wp_admin_bar->add_menu(array(
+                'id' => 'manage_alerts_link',
+                'title' => __('<span class="ab-icon dashicons-before dashicons-megaphone"></span> Alerts'),
+                'href' => __(site_url() . "/#/manage-alerts"),
+                'parent' => 'manage_link',
+            ));
+        }
     } else {
-        $wp_admin_bar->add_menu( array(
+        $wp_admin_bar->add_menu(array(
             'id' => 'manage_profile_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-welcome-write-blog"></span> Edit Profile'),
-            'href' => __(site_url()."/#/manage-my-profile"),
+            'title' => __('<span class="ab-icon dashicons-before dashicons-welcome-write-blog"></span> Edit Profile'),
+            'href' => __(site_url() . "/#/manage-my-profile"),
         ));
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_ONESEARCH)) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_onesearch_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-search"></span> oneSearch'),
-            'href' => __(site_url()."/#/manage-onesearch"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_ADMIN )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_users_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-lock"></span> Permissions'),
-            'href' => __(site_url()."/#/manage-user-groups"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_HOURS )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_hours_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-clock"></span> Library Hours'),
-            'href' => __(site_url()."/#/manage-hours"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_STAFFDIR )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_staffdir_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-groups"></span> Staff Directory'),
-            'href' => __(site_url()."/#/manage-staff-directory"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_DATABASES )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_databases_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-networking"></span> Databases'),
-            'href' => __(site_url()."/#/manage-databases"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_SOFTWARE )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_software_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-analytics"></span> Software'),
-            'href' => __(site_url()."/#/manage-software"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_FORMS )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_forms_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-clipboard"></span> Forms'),
-            'href' => __(site_url()."/#/manage-forms"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_NEWS )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_news_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-calendar"></span> News &amp; Exhibits'),
-            'href' => __(site_url()."/#/manage-news"),
-            'parent' => 'manage_link',
-        ) );
-    }
-    if (($wpUser = gDoesUserHaveAccessWP( GROUP_ALERTS )) !== false) {
-        $wp_admin_bar->add_menu( array(
-            'id' => 'manage_alerts_link',
-            'title' => __( '<span class="ab-icon dashicons-before dashicons-megaphone"></span> Alerts'),
-            'href' => __(site_url()."/#/manage-alerts"),
-            'parent' => 'manage_link',
-        ) );
     }
 }
 add_action('admin_bar_menu', 'add_manage_admin_bar_link',999);
