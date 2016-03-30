e1338f9b0d3a2a9a5ba59c00e10c5d21c59d83e0
IMU-Drew <IMU-Drew@users.noreply.github.com>
2016-03-30 17:08:25 -0500

    Breadcrumb test 8

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,9 +1,10 @@
 <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
     <?php if(function_exists('bcn_display'))
     {
-        $display = bcn_display(true);
-        echo $display;
+        bcn_display();
     }?>
 </div>
+<?php the_content(); ?>
+<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php the_content(); ?>
 <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
