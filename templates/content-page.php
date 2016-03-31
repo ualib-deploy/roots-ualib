68efeeb1c45835493fc9377bf79872e71e9efec6
amparker8 <andrew-parker-1@uiowa.edu>
2016-03-31 11:07:30 -0500

    Shifting breadcrumb location

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,8 +1,2 @@
-<div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
-    <?php if(function_exists('bcn_display'))
-    {
-        bcn_display();
-    }?>
-</div>
 <?php the_content(); ?>
 <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
