95cfba5e200d91908216387ee9a84e7e0ab3aa67
IMU-Drew <IMU-Drew@users.noreply.github.com>
2016-03-31 11:45:50 -0500

    Jumbotron header breadcrumb test 4

--- templates/jumbotron-header.php
+++ templates/jumbotron-header.php
@@ -1,7 +1,7 @@
 <?php $fields = get_fields(); ?>
 <?php $jumboHeadBg = isset($fields['header_image']['url']) ? $fields['header_image']['url'] : false; ?>
 <div class="jumbotron-header" <?php if ($jumboHeadBg !== false) print 'style="background-image: url('.$jumboHeadBg.');"'; ?>>
-    <div class="jumbotron">
+    <div class="jumbotron drewtest">
         <div class="container">
             <?php print $fields['header_content']; ?>
         </div>
