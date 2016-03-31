ecbdeaabc93fa0d342dac752332f78f7cc7706fa
IMU-Drew <IMU-Drew@users.noreply.github.com>
2016-03-31 14:11:20 -0500

    Jumbotron header breadcrumb test 5

--- templates/jumbotron-header.php
+++ templates/jumbotron-header.php
@@ -1,8 +1,15 @@
 <?php $fields = get_fields(); ?>
 <?php $jumboHeadBg = isset($fields['header_image']['url']) ? $fields['header_image']['url'] : false; ?>
+
 <div class="jumbotron-header" <?php if ($jumboHeadBg !== false) print 'style="background-image: url('.$jumboHeadBg.');"'; ?>>
     <div class="jumbotron drewtest">
         <div class="container">
+              <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+                  <?php if(function_exists('bcn_display'))
+                  {
+                      bcn_display();
+                  }?>
+              </div>
             <?php print $fields['header_content']; ?>
         </div>
     </div>
