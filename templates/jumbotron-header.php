616644bea8d7d1852b88c1e5a4a645d0c87c2a22
IMU-Drew <IMU-Drew@users.noreply.github.com>
2016-04-05 14:51:54 -0500

    Add bootstrap markup 4

--- templates/jumbotron-header.php
+++ templates/jumbotron-header.php
@@ -7,7 +7,7 @@
               <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
                   <?php if(function_exists('bcn_display'))
                   {
-                      bcn_display();
+                      //bcn_display();
                   }?>
               </div>
             <?php print $fields['header_content']; ?>
