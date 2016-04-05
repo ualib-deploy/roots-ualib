81d433bb8c9ae1f96433b1555a1a4f9712cedbe8
IMU-Drew <IMU-Drew@users.noreply.github.com>
2016-04-05 14:35:26 -0500

    Add bootstrap markup 3

--- templates/page-header.php
+++ templates/page-header.php
@@ -5,8 +5,6 @@
     <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
         <?php if(function_exists('bcn_display'))
         {
-            $display = bcn_display(true);
-            print_r($display); 
             print_r("Testing...")
         }?>
     </div>
