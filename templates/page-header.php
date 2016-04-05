54bd89d2530ee2b1235b3546fa3a1d0a39e6f023
IMU-Drew <IMU-Drew@users.noreply.github.com>
2016-04-05 15:21:46 -0500

    Add bootstrap markup 12

--- templates/page-header.php
+++ templates/page-header.php
@@ -5,9 +5,7 @@
     <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
         <?php if(function_exists('bcn_display'))
         {
-            bcn_display();
             $display = bcn_display(true);
-            str_replace(' > ', '', $display); 
             $display = '<ol class="breadcrumb">' . $display . '</ol>';
             print($display);
         }?>
