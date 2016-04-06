6efd897f216b1c68d2afa3c42b3651c8aab23d79
IMU-Drew <IMU-Drew@users.noreply.github.com>
2016-04-06 08:50:19 -0500

    Replace <div class=breadcrumbs> 2

--- templates/jumbotron-header.php
+++ templates/jumbotron-header.php
@@ -4,14 +4,14 @@
 <div class="jumbotron-header" <?php if ($jumboHeadBg !== false) print 'style="background-image: url('.$jumboHeadBg.');"'; ?>>
     <div class="jumbotron drewtest">
         <div class="container">
-              <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+              <ol class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
                   <?php if(function_exists('bcn_display'))
                   {
                     $display = bcn_display(true);
-                    $display = '<ol class="breadcrumb">' . $display . '</ol>';
+                    //$display = '<ol class="breadcrumb">' . $display . '</ol>';
                     print($display);
                   }?>
-              </div>
+              </ol>
             <?php print $fields['header_content']; ?>
         </div>
     </div>
