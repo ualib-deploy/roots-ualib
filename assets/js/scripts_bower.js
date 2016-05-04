e75a2b0fed04f09e3b63f9d5cf501d29a45414c7
amparker8 <andrew-parker-1@uiowa.edu>
2016-05-04 10:55:10 -0500

    Tentative Scout check box

--- assets/js/scripts_bower.js
+++ assets/js/scripts_bower.js
@@ -3020,6 +3020,11 @@ angular.module('ualib.compfinder.admin', [
                     console.dir(data);
                 });
         };
+        $scope.updateComputer = function(computer) {
+            var compArray = [];
+            compArray.push(computer);
+            $scope.updateComputers(compArray);
+        };
 
         $scope.validateBldg = function(building) {
             if (building.name.length < 1) {
@@ -20119,8 +20124,11 @@ angular.module("common/directives/suggest/suggest.tpl.html", []).run(["$template
     "           ng-model=\"model\" ng-change=\"onChange()\" ng-trim=\"false\" autocomplete=\"off\" />\n" +
     "    <div class=\"input-group-btn\">\n" +
     "        <button type=\"submit\" class=\"btn btn-onesearch btn-primary\"><span class=\"fa fa-search\"></span></button>\n" +
+    "\n" +
     "    </div>\n" +
+    "\n" +
     "</div>\n" +
+    "<div class=\"input-group-checkbox\" id=\"scoutCheckbox\"><span style=\"font-size: 11pt; position:relative; top:-2px;\">Only search Scout</span>&nbsp;<input type=\"checkbox\" ng-model=\"scoutCheckbox\" ng-change=\"checkboxChange()\"></input></div>\n" +
     "<div class=\"suggest\" ng-show=\"showSuggestions && selected && (items.suggest.length > 0 || items.recommend.length > 0 || items.subjects[0].subjects.length > 0 || items.faq.length > 0)\">\n" +
     "    <div ng-if=\"items.suggest.length > 0\">\n" +
     "        <ul class=\"nav nav-pills nav-stacked\">\n" +
@@ -20331,7 +20339,9 @@ angular.module("common/engines/scout/scout.tpl.html", []).run(["$templateCache",
     "        <div collapse=\"isCollapsed\" ng-show=\"(item.RecordInfo.BibRecord.BibRelationships.HasContributorRelationships || item.source || item.RecordInfo.BibRecord.BibEntity.Subjects)\">\n" +
     "            <div class=\"details-container\" ng-if=\"item.RecordInfo.BibRecord.BibRelationships.HasContributorRelationships\">\n" +
     "                <span class=\"text-muted\">Authors </span>\n" +
-    "            <span class=\"details\"\n" +
+    "            <span class=\"details\"...\n" +
+    "                  ..\n" +
+    "\n" +
     "                  ng-repeat=\"author in item.RecordInfo.BibRecord.BibRelationships.HasContributorRelationships | unique: 'PersonEntity.Name.NameFull'\"\n" +
     "                  ng-bind-html=\"author.PersonEntity.Name.NameFull | lowercase | ucfirst\"></span>\n" +
     "            </div>\n" +
@@ -21053,7 +21063,7 @@ angular.module('oneSearch.common')
                 model: '=',
                 search: '='
             },
-            controller: ['$scope', '$window', '$timeout', '$document', 'dataFactory', function($scope, $window, $timeout, $document,  dataFactory){
+            controller: ['$scope', '$window', '$timeout', '$document', 'dataFactory', 'Bento', function($scope, $window, $timeout, $document,  dataFactory, Bento){
                 $scope.items = {};
                 $scope.filteredItems = [];
                 $scope.model = "";
@@ -21066,8 +21076,16 @@ angular.module('oneSearch.common')
                 // hides the list initially
                 $scope.selected = false;
 
+                $scope.checkboxChange = function(){
+                    console.log("CHECKBOX CHANGE CALLED;")
+                    Bento.getBoxes();
+                };
+
                 $scope.onChange = function(){
                     //console.log("OnChange event.");
+
+                    //window.location = 'http://search.ebscohost.com/login.aspx?direct=true&site=eds-live&;scope=site&type=0&custid=s4594951&groupid=main&profid=eds&mode=and&authtype=ip,guest&bquery=test'; //params['s']
+
                     $scope.selected = true;
                     var fixedString = $scope.model.replace(/[&\/\\#+()$~%':*?<>{}]/g, ' ').trim();
                     fixedString = fixedString.substring(0, 150);
@@ -22433,6 +22451,13 @@ angular.module('common.oneSearch', [])
 
         function search(params){
 
+            var checkbox = document.querySelector('#scoutCheckbox input');
+            if (checkbox.checked) {
+                window.location = 'http://search.ebscohost.com/login.aspx?direct=true&site=eds-live&;scope=site&type=0&custid=s4594951&groupid=main&profid=eds&mode=and&authtype=ip,guest&bquery=' + params['s'];
+            }
+
+            console.log("TEST");
+
             var canceller = $q.defer();
             var url = '//wwwdev2.lib.ua.edu/oneSearch/api/search/' + params['s'] + '/engine/' + params['engine'] + '/limit/' + params['limit'];
 
@@ -22769,6 +22794,7 @@ angular.module('common.oneSearch', [])
          *
          * **Note:** This function will route relative to UA Libraries' `live` and `dev` URLs. If this function is executed outside a UALib domain, `www.lib.ua.edu` will be used by default.
          */
+
 
         $scope.search = function(){
             /**
