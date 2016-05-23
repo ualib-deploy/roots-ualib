1a4e08ed7a355b0b2e521c16771a6fef87c8d149
Will Jones <the8bitsquid@gmail.com>
2016-05-23 15:56:07 -0500

    fix(manage_ui): Fix for databases management UI

--- assets/js/scripts_bower.js
+++ assets/js/scripts_bower.js
@@ -13679,6 +13679,7 @@ angular.module('manage.manageDatabases', [])
             $scope.$watchCollection('filters', processFilters);
 
             function processFilters(newVal, oldVal){
+                newVal = angular.isUndefined(newVal) ? angular.copy($scope.filters) : newVal;
                 var db = angular.copy(DBList.databases);
 
                 for(var filter in newVal){
@@ -13733,6 +13734,9 @@ angular.module('manage.manageDatabases', [])
                             if (data == 1){
                                 //$scope.DBList.databases.splice($scope.DBList.databases.indexOf(db), 1);
                                 $scope.filteredDB.splice($scope.filteredDB.indexOf(db), 1);
+                                DBList.databases = DBList.databases.filter(function(item){
+                                    return item.id !== db.id;
+                                });
                                 $scope.formResponse = "Database has been deleted.";
                             } else {
                                 $scope.formResponse = "Error: Can not delete database! " + data;
@@ -13787,12 +13791,11 @@ angular.module('manage.manageDatabases', [])
 
 
             $scope.createDB = function(){
-                console.dir($scope.newDB);
                 mdbFactory.postData({action : 3}, $scope.newDB)
                     .success(function(data, status, headers, config) {
                         if ((typeof data === 'object') && (data !== null)){
                             var newDB = {};
-                            newDB = angular.copy($scope.newDB);
+                            newDB = angular.extend({publisher: '', vendor:'', disabled: 0}, $scope.newDB);
                             newDB.id = data.id;
                             newDB.subjects = angular.copy(data.subjects);
                             newDB.types = angular.copy(data.types);
@@ -13803,7 +13806,7 @@ angular.module('manage.manageDatabases', [])
                             newDB.subjType = 1;
                             newDB.selType = data.types[0];
                             DBList.databases.push(newDB);
-                            processFilters($scope.filters);
+                            processFilters();
                             $scope.formResponse = "Database has been created.";
                         } else {
                             $scope.formResponse = "Error: Can not create database! " + data;
