7819a997f13cc80a0f95e853377cd1eabded57cf
amparker8 <andrew-parker-1@uiowa.edu>
2016-11-02 14:50:05 -0500

    Dev build: Research help demo

--- assets/js/scripts_bower.js
+++ assets/js/scripts_bower.js
@@ -17718,7 +17718,7 @@ angular.module('ualib.musicSearch')
 
 
 
-angular.module('oneSearch.templates', ['bento/bento.tpl.html', 'common/directives/suggest/suggest.tpl.html', 'common/engines/acumen/acumen.tpl.html', 'common/engines/catalog/catalog.tpl.html', 'common/engines/databases/databases.tpl.html', 'common/engines/ejournals/ejournals.tpl.html', 'common/engines/google-cs/google-cs.tpl.html', 'common/engines/recommend/recommend.tpl.html', 'common/engines/scout/scout.tpl.html']);
+angular.module('oneSearch.templates', ['bento/bento.tpl.html', 'common/directives/suggest/suggest.tpl.html', 'common/engines/acumen/acumen.tpl.html', 'common/engines/catalog/catalog.tpl.html', 'common/engines/databases/databases.tpl.html', 'common/engines/ejournals/ejournals.tpl.html', 'common/engines/google-cs/google-cs.tpl.html', 'common/engines/recommend/recommend.tpl.html', 'common/engines/scout/scout.tpl.html', 'common/engines/staff-directory/staff-directory.tpl.html']);
 
 angular.module("bento/bento.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("bento/bento.tpl.html",
@@ -17748,7 +17748,7 @@ angular.module("bento/bento.tpl.html", []).run(["$templateCache", function($temp
     "                </h2>\n" +
     "            </div>\n" +
     "        </div>\n" +
-    "        <div class=\"col-sm-12 col-md-4\">\n" +
+    "        <div class=\"col-md-4\">\n" +
     "            <div class=\"bento-box\" bento-box=\"journals\">\n" +
     "                <h2>\n" +
     "                    Journals\n" +
@@ -17756,6 +17756,32 @@ angular.module("bento/bento.tpl.html", []).run(["$templateCache", function($temp
     "                        <span class=\"fa fa-info-circle\"\n" +
     "                              tooltip-placement=\"right\"\n" +
     "                              tooltip=\"Keyword search in journal titles and journal collections, in both Scout and our E-Resources.\"></span>\n" +
+    "                    </small>\n" +
+    "                </h2>\n" +
+    "            </div>\n" +
+    "        </div><!--\n" +
+    "        <div class=\"col-sm-12 col-md-4\">\n" +
+    "            <div class=\"bento-box\" bento-box=\"staffdirectory\">\n" +
+    "                <h2>\n" +
+    "                    Research Help\n" +
+    "                    <small>\n" +
+    "                        <span class=\"fa fa-info-circle\"\n" +
+    "                              tooltip-placement=\"right\"\n" +
+    "                              tooltip=\"Contact a librarian directly for help with your research.\"></span>\n" +
+    "                    </small>\n" +
+    "                </h2>\n" +
+    "            </div>\n" +
+    "        </div>-->\n" +
+    "    </div>\n" +
+    "    <div class=\"row\">\n" +
+    "        <div class=\"col-md-12\">\n" +
+    "            <div class=\"bento-box\" hide-if-empty=\"true\" bento-box=\"staffdirectory\" style=\"min-height:0px; margin-bottom: 15px;\">\n" +
+    "                <h2>\n" +
+    "                    Research Help\n" +
+    "                    <small>\n" +
+    "                            <span class=\"fa fa-info-circle\"\n" +
+    "                                  tooltip-placement=\"right\"\n" +
+    "                                  tooltip=\"Contact a librarian directly for help with your research.\"></span>\n" +
     "                    </small>\n" +
     "                </h2>\n" +
     "            </div>\n" +
@@ -17788,16 +17814,17 @@ angular.module("bento/bento.tpl.html", []).run(["$templateCache", function($temp
     "        </div>\n" +
     "        <div class=\"col-md-4\">\n" +
     "            <div class=\"bento-box\" bento-box=\"acumen\">\n" +
-    "                <h2 id=\"acumen\">\n" +
-    "                    Acumen <small>Digital Archives</small>\n" +
+    "                <h2>\n" +
+    "                    Acumen\n" +
     "                    <small>\n" +
     "                        <span class=\"fa fa-info-circle\"\n" +
     "                              tooltip-placement=\"right\"\n" +
-    "                              tooltip=\"Returns a keyword search in titles and full text of our Special Collections Digital Archive.\"></span>\n" +
+    "                              tooltip=\"Uses Google API to run a keyword search in titles and full text for our research guides.\"></span>\n" +
     "                    </small>\n" +
     "                </h2>\n" +
     "            </div>\n" +
     "        </div>\n" +
+    "\n" +
     "    </div>\n" +
     "    <div class=\"row\">\n" +
     "        <div class=\"col-md-4\">\n" +
@@ -17829,13 +17856,14 @@ angular.module("bento/bento.tpl.html", []).run(["$templateCache", function($temp
     "                <h2>\n" +
     "                    Research Guides\n" +
     "                    <small>\n" +
-    "                        <span class=\"fa fa-info-circle\"\n" +
-    "                              tooltip-placement=\"right\"\n" +
-    "                              tooltip=\"Uses Google API to run a keyword search in titles and full text for our research guides.\"></span>\n" +
+    "                            <span class=\"fa fa-info-circle\"\n" +
+    "                                  tooltip-placement=\"right\"\n" +
+    "                                  tooltip=\"Uses Google API to run a keyword search in titles and full text for our research guides.\"></span>\n" +
     "                    </small>\n" +
     "                </h2>\n" +
     "            </div>\n" +
     "        </div>\n" +
+    "    </div>\n" +
     "    </div>\n" +
     "    <div class=\"row\">\n" +
     "        <div class=\"col-md-12\">\n" +
@@ -18101,6 +18129,39 @@ angular.module("common/engines/scout/scout.tpl.html", []).run(["$templateCache",
     "\n" +
     "    </div>\n" +
     "</div>");
+}]);
+
+angular.module("common/engines/staff-directory/staff-directory.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("common/engines/staff-directory/staff-directory.tpl.html",
+    "<div class=\"col-md-4\">\n" +
+    "    <div class=\"media\">\n" +
+    "        <div class=\"media-left\">\n" +
+    "            <a href=\"#\">\n" +
+    "                <img class=\"media-object\" style=\"width: 128px;\" src='https://wwwdev2.lib.ua.edu/staffDir/staffImages/{{item.photo}}' >\n" +
+    "            </a>\n" +
+    "        </div>\n" +
+    "        <div class=\"media-body\">\n" +
+    "            <div class=\"media-heading\">\n" +
+    "                <ul class=\"list-unstyled\">\n" +
+    "                    <li><a ng-href=\"#/staffdir/{{item.emailPrefix}}\" ng-if=\"item.profile\">\n" +
+    "                        {{item.firstName}} {{item.lastName}}\n" +
+    "                    </a></li>\n" +
+    "                    <li ng-if=\"!item.profile\">{{item.firstName}} {{item.lastName}} </li>\n" +
+    "                    <li>{{item.subject}}</li>\n" +
+    "\n" +
+    "                    <li>{{item.title}}</li>\n" +
+    "                    <!--<div class=\"media-heading\">{{item.department}}</div>-->\n" +
+    "                    <li><a href=\"mailto:{{item.email}}\">{{item.email}}</a></li>\n" +
+    "                </ul>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "");
 }]);
 
 /**
@@ -18370,7 +18431,6 @@ angular.module('oneSearch.bento', [])
                     if ((box == 'articles') || (box == 'databases')){
                         self.boxes[box].resultLimit = 6;
                     }
-
                     else if (box == 'journals'){
                         if (expecting < 2 && self.boxes[box].resultLimit == 3){
                             self.boxes[box].resultLimit = 6;
@@ -18796,7 +18856,7 @@ angular.module('oneSearch.common')
                 model: '=',
                 search: '='
             },
-            controller: ['$scope', '$window', '$timeout', '$document', 'dataFactory', 'Bento', function($scope, $window, $timeout, $document,  dataFactory, Bento){
+            controller: function($scope, $window, $timeout, $document,  dataFactory, Bento){
                 $scope.items = {};
                 $scope.filteredItems = [];
                 $scope.model = "";
@@ -18920,8 +18980,7 @@ angular.module('oneSearch.common')
                 };
 
 
-
-            }],
+            },
             link: function(scope, elem, attrs) {
                 scope.showSuggestions = false;
                 var suggestWatcher = scope.$watch('items', function(newVal, oldVal){
@@ -19074,7 +19133,7 @@ angular.module('engines.acumen', [])
      * <mark>TODO:</mark>   add proper description.
      */
 
-    .controller('AcumenCtrl', ['$scope', '$filter', function($scope, $filter){
+    .controller('AcumenCtrl', function($scope, $filter){
         var items = $scope.items;
 
         for (var i = 0, len = items.length; i < len; i++) {
@@ -19084,7 +19143,7 @@ angular.module('engines.acumen', [])
                 else items[i].type = items[i].type.sort().shift();
             }
         }
-    }]);
+    });
 angular.module('engines.catalog', [])
 
     /**
@@ -19145,7 +19204,7 @@ angular.module('engines.catalog', [])
      * <mark>TODO:</mark>   add proper description.
      */
 
-    .controller('CatalogCtrl', ['$scope', '$filter', function($scope, $filter){
+    .controller('CatalogCtrl', function($scope, $filter){
         var types = {
             bc: "Archive/Manuscript",
             cm: "Music Score",
@@ -19187,7 +19246,7 @@ angular.module('engines.catalog', [])
         }
 
         $scope.items = items;
-    }]);
+    });
 
 angular.module('engines.databases', [])
 
@@ -19271,7 +19330,7 @@ angular.module('engines.ejournals', [])
      * <mark>TODO:</mark>   add proper description.
      */
 
-    .controller('EjouralsCtrl', ['$scope', function($scope){
+    .controller('EjouralsCtrl', function($scope){
 
         var param;
         switch ($scope.mediaType){
@@ -19288,7 +19347,7 @@ angular.module('engines.ejournals', [])
         if (param){
             $scope.resourceLink = $scope.resourceLink.replace('SS_searchTypeAll=yes&SS_searchTypeBook=yes&SS_searchTypeJournal=yes&SS_searchTypeOther=yes', param);
         }
-    }]);
+    });
 /**
  * @ngdoc overview
  * @name engines
@@ -19357,7 +19416,8 @@ angular.module('common.engines', [
     'engines.faq',
     'engines.libguides',
     'engines.ejournals',
-    'engines.recommend'
+    'engines.recommend',
+    'engines.staffdirectory'
 ])
 /**
  * @Service enginesTemplateFactory
@@ -19636,7 +19696,7 @@ angular.module('engines.scout', [])
      * <mark>TODO:</mark>   add proper description.
      */
 
-    .controller('ScoutCtrl', ['$scope', function($scope){
+    .controller('ScoutCtrl', function($scope){
         var title; // Title variable to bind to $scope. ".BibRelationships.IsPartOfRelationships" title is used if no item title is present.
         var items = $scope.items;
         for (var i = 0; i < items.length; i++){
@@ -19710,7 +19770,51 @@ angular.module('engines.scout', [])
         }
 
         $scope.resourceLink = angular.copy(link);
-    }]);
+    });
+/**
+ * @ngdoc object
+ * @name engines.type:ENGIEN_NAME
+ *
+ * @description
+ * Engine config properties
+ *
+ * | property | value |
+ * |----------|-------|
+ * | id       | 128      |
+ * | priority | 5      |
+ * | resultsPath | StaffDirectory     |
+ * | templateUrl | common/engines/recommend/staff-directory.tpl.html|
+ * | controller |  N/A  |
+ *
+ * @requires oneSearchProvider
+ */
+
+angular.module('engines.staffdirectory', [])
+    .config(['oneSearchProvider', function(oneSearchProvider){
+        oneSearchProvider.engine('staffdirectory', {
+            id: 128,
+            priority: 5,
+            resultsPath: 'staffDir',
+            templateUrl: 'common/engines/staff-directory/staff-directory.tpl.html',
+            controller: 'StaffDirectoryCtrl'
+        })
+    }])
+.controller('StaffDirectoryCtrl', function($scope){
+
+    var items = $scope.items;
+
+    for (var i = 0, len = items.length; i < len; i++) {
+
+        if (items[i].email) {
+            //console.log(items[i].type);
+            var rx = /^([\w-]+(?:\.[\w-]+)*)/;
+            var prefix = items[i].email.match(rx);
+            if (prefix !== null) {
+                items[i].emailPrefix = prefix[0];
+            }
+        }
+    }
+});
 angular.module('filters.nameFilter', [])
 
     .filter('nameFilter', ['$filter', function($filter){
@@ -20533,13 +20637,13 @@ angular.module('common.oneSearch', [])
              * The $scope model for the search string, bound to the input text box.
              */
 
+
             //Redirect to Scout if "Only search Scout" is checked
             var checkbox = $scope.scoutCheckbox;
             var searchtext = $scope.searchText;
 
-
             if ((checkbox == true) && (searchtext !== '')) {
-                ga('send', 'event', 'oneSearch', 'scout_checkbox_click');
+                //ga('send', 'event', 'oneSearch', 'scout_checkbox_click');
                 window.location = 'http://search.ebscohost.com/login.aspx?direct=true&site=eds-live&;scope=site&type=0&custid=s4594951&groupid=main&profid=eds&mode=and&authtype=ip,guest&bquery=' + searchtext;
             }
             else {
@@ -20893,7 +20997,7 @@ factory(root.angular);
 }
 }(this, function(angular) {
 /**
- * AngularJS Google Maps Ver. 1.17.3
+ * AngularJS Google Maps Ver. 1.17.7
  *
  * The MIT License (MIT)
  * 
@@ -21094,6 +21198,8 @@ angular.module('ngMap', []);
         ((typeof center === 'string') && center.match(/\{\{.*\}\}/))
       ) {
         mapOptions.center = new google.maps.LatLng(0, 0);
+      } else if( (typeof center === 'string') && center.match(/[0-9.-]*,[0-9.-]*/) ){
+           mapOptions.center = new google.maps.LatLng(center);
       } else if (!(center instanceof google.maps.LatLng)) {
         var geoCenter = mapOptions.center;
         delete mapOptions.center;
@@ -21405,9 +21511,9 @@ angular.module('ngMap', []);
       position && (this.position = position); /* jshint ignore:line */
 
       if (this.getProjection() && typeof this.position.lng == 'function') {
-        var posPixel = this.getProjection().fromLatLngToDivPixel(this.position);
         var _this = this;
         var setPosition = function() {
+          var posPixel = _this.getProjection().fromLatLngToDivPixel(_this.position);
           var x = Math.round(posPixel.x - (_this.el.offsetWidth/2));
           var y = Math.round(posPixel.y - _this.el.offsetHeight - 10); // 10px for anchor
           _this.el.style.left = x + "px";
@@ -23373,7 +23479,7 @@ angular.module('ngMap', []);
 
       // convert output more for center and position
       if (
-        (options.key == 'center' || options.key == 'center') &&
+        (options.key == 'center' || options.key == 'position') &&
         output instanceof Array
       ) {
         output = new google.maps.LatLng(output[0], output[1]);
