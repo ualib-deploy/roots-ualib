f3fb4128461c4f1376fdf117c7fc458c63eed0e7
amparker8 <andrew-parker-1@uiowa.edu>
2017-02-08 11:43:20 -0600

    Dev build: permissions app fix in progress 3

--- assets/js/scripts_bower.js
+++ assets/js/scripts_bower.js
@@ -16074,15 +16074,13 @@ angular.module('manage.manageUserGroups', [])
         wpUsersFactory.getAllUsersWP()
             .success(function(data) {
                 //remove admin accounts
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i].last_name.length < 1) {
-                        data.splice(i, 1);
+                for (i = 0; i < data.length; i++) {
+                    nameArray = data[i].name.split(' ');
+                    if (nameArray.length > 1) {
+                        data[i].fullName = data[i].name;
                     }
                 }
 
-                for (i = 0; i < data.length; i++) {
-                    data[i].fullName = data[i].last_name + ", " + data[i].first_name + " (" + data[i].nickname + ")";
-                }
                 $scope.wpUsers = data;
                 $scope.newUser = $scope.wpUsers[0];
                 console.log("WP user list received:");
