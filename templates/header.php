36e05add3cc5a607ebaa6d51579af6717c438725
amparker8 <andrew-parker-1@uiowa.edu>
2017-07-19 10:40:37 -0500

    Remove music library from megamenu and internal Hours page

--- templates/header.php
+++ templates/header.php
@@ -173,7 +173,6 @@
                                                 <span class="fa fa-music"></span>
                                                 <h4>Music Library</h4>
                                                 <ul>
-                                                    <li class="menu-video-database-search"><a href="/#/videos">Video Database Search</a></li>
                                                     <li class="menu-research-guides"><a href="http://guides.lib.ua.edu/visualperformingarts">Research Guides</a></li>
                                                     <li class="menu-home"><a href="/libraries/music/">Music Library Home</a></li>
                                                 </ul>
