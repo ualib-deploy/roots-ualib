0b9204984a5e87331574e1af055356f20da9eb95
amparker8 <andrew-parker-1@uiowa.edu>
2017-11-02 10:56:18 -0500

    Dev build: small additional heading, style changes

--- assets/js/scripts_bower.js
+++ assets/js/scripts_bower.js
@@ -28153,7 +28153,7 @@ angular.module("news-item/event-card.tpl.html", []).run(["$templateCache", funct
     "        </div>\n" +
     "    </div>\n" +
     "    <div class=\"media-body\">\n" +
-    "        <h4 class=\"media-heading\" ng-bind-html=\"newsCard.title | truncate:50:'...':true\"></h4>\n" +
+    "        <h2 class=\"h4 media-heading\" ng-bind-html=\"newsCard.title | truncate:50:'...':true\"></h2>\n" +
     "        <p ng-bind-html=\"newsCard.blurb | truncate:150:'...':true\"></p>\n" +
     "    </div>\n" +
     "</a>");
@@ -28163,7 +28163,7 @@ angular.module("news-item/news-card.tpl.html", []).run(["$templateCache", functi
   $templateCache.put("news-item/news-card.tpl.html",
     "<a ng-href=\"#/news-exhibits/{{newsCard.link}}\" class=\"media news-card\">\n" +
     "    <div class=\"media-body\">\n" +
-    "        <h2 class=\"h4\" class=\"media-heading\">\n" +
+    "        <h2 class=\"h4 media-heading\">\n" +
     "            <span ng-bind-html=\"newsCard.title | truncate:50:'...':true\"></span>\n" +
     "        </h2>\n" +
     "        <div class=\"details-context\" ng-if=\"(newsCard.activeFrom != newsCard.activeUntil && newsCard.type != 0)\">\n" +
