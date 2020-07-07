8e28a2dcd82b160293964c6e96d65c15e5861279
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-07-07 12:25:28 -0500

    add conditional logic for form result pages

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,32 @@
 <?php the_content(); ?>
+<?php
+
+      if(is_page( array('circ-trace-form-result',
+'studentemployeerequest-result', 'incidentreport-result', 'actcardform-result',
+'voyager-request-result') ))
+        require_once get_stylesheet_directory() . '/intranet/assets/incidentreport/class.phpmailer.php';
+
+      if(is_page('circ-trace-form-result'))
+        require_once get_stylesheet_directory() . '/intranet/inc/circ-trace-form-result.php';
+
+      if(is_page('studentemployeerequest-result'))
+        require_once get_stylesheet_directory() . '/intranet/inc/studentemployeerequest-result.php';
+
+      if(is_page('actcardform-result'))
+        require_once get_stylesheet_directory() . '/intranet/inc/actcardform-result.php';
+
+      if(is_page('voyager-request')) {
+        require_once get_stylesheet_directory() . '/intranet/inc/openfiles.php';
+        require_once get_stylesheet_directory() . '/intranet/inc/voyager-request.php';
+      }
+
+      if(is_page('voyager-request-result'))
+        require_once get_stylesheet_directory() . '/intranet/inc/voyager-request-result.php';
+
+      if(is_page('incidentreport-result')) {
+        /* require_once get_stylesheet_directory() . '/assets/incidentreport/class.inputfilter.php'; */
+        require_once get_stylesheet_directory() . '/intranet/inc/incidentreport-result.php';
+      }
+
+    ?>
 <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
