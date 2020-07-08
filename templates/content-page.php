ef293045c54d93d2e7970837ad9b360ebb2c5110
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-07-08 15:34:38 -0500

    add conditional to check site url, update script paths with grunt build dest filepaths

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,5 +1,12 @@
 <?php the_content(); ?>
 <?php
+      $current_url = home_url( '/' );
+
+      if ($current_url == 'https://wwwdev2.lib.ua.edu/') {
+          $intranet_path = '/intranet/inc';
+      } else if ($current_url == 'https://www.lib.ua.edu/') {
+          $intranet_path = '/intranet/inc/dest';
+      }
 
       if(is_page( array('circ-trace-form-result',
 'studentemployeerequest-result', 'incidentreport-result', 'actcardform-result',
@@ -7,13 +14,13 @@
         require_once get_stylesheet_directory() . '/intranet/assets/incidentreport/class.phpmailer.php';
 
       if(is_page('circ-trace-form-result'))
-        require_once get_stylesheet_directory() . '/intranet/inc/circ-trace-form-result.php';
+        require_once get_stylesheet_directory() . $intranet_path . '/circ-trace-form-result.php';
 
       if(is_page('studentemployeerequest-result'))
-        require_once get_stylesheet_directory() . '/intranet/inc/studentemployeerequest-result.php';
+        require_once get_stylesheet_directory() . $intranet_path . '/studentemployeerequest-result.php';
 
       if(is_page('actcardform-result'))
-        require_once get_stylesheet_directory() . '/intranet/inc/actcardform-result.php';
+        require_once get_stylesheet_directory() . $intranet_path . '/actcardform-result.php';
 
       if(is_page('voyager-request')) {
         require_once get_stylesheet_directory() . '/intranet/inc/openfiles.php';
@@ -21,7 +28,7 @@
       }
 
       if(is_page('voyager-request-result'))
-        require_once get_stylesheet_directory() . '/intranet/inc/voyager-request-result.php';
+        require_once get_stylesheet_directory() . $intranet_path . '/voyager-request-result.php';
 
       if(is_page('incidentreport-result')) {
         require_once get_stylesheet_directory() . '/intranet/inc/incidentreport-result.php';
