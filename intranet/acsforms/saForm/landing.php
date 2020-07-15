ce15b2e970244b920aa9458c30e977735b2d6096
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-07-15 14:16:01 -0500

    change landing to php instead of html

---
+++ intranet/acsforms/saForm/landing.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+    
+    <!-- <link rel="stylesheet" type="text/css" href="stylesheet.css"> -->     
+        
+	
+    
+	
+		<div class="container">
+			<img src="images/librariesk_black.png" alt="UA Libraries Logo" style="width:150px;height:60px">
+				<h1 class="h2">Security Authorization Form</h1>
+				<h2 class="h3">Network & Voyager Access</h2>
+		<hr>
+		
+		<p>Form Submitted Successfully. Please allow 2 - 3 business days for processing.</p>
+		
+		</div>
+
+		
+
+
