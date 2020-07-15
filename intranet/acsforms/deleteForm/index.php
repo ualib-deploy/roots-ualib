1b460d627e98f3ab9dcb1250b22cfe096d785cc6
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-07-15 14:49:49 -0500

    delete irrelevant html tags, styles, scripts, and files

--- intranet/acsforms/deleteForm/index.php
+++ intranet/acsforms/deleteForm/index.php
@@ -1,48 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-
-    <!-- Required meta tags always come first -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">     
         
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	
-    <!-- Javascript -->
-    
-	<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-    <script type='text/javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<script>
-			jQuery(document).ready(function($){
-				$('#AssociateDean').change(function(obj){
-			
-					if ($(this).val() == 'Tom Wilson'){
-						$('#AssociateDeanPhone').val('2299');
-						$('#AssociateDeanEmail').val('tcwilson@ua.edu');
-					}
-					if ($(this).val() == 'Millie Jackson'){
-						$('#AssociateDeanPhone').val('5008');
-						$('#AssociateDeanEmail').val('mljackson@ua.edu');
-					}
-					if ($(this).val() == 'Lorraine Madway'){
-						$('#AssociateDeanPhone').val('0513');
-						$('#AssociateDeanEmail').val('lmadway@ua.edu');
-					}
-					if ($(this).val() == 'Emy Decker'){
-						$('#AssociateDeanPhone').val('3497');
-						$('#AssociateDeanEmail').val('endecker@ua.edu');
-					}
-				});
-			});
-		</script>
-  <title>Deleting Network and Voyager Access</title>
-  </head>
-  
-  <body>
-      <div class="container">
+  <div class="container">
 		<form action="https://imageweb.ua.edu/Public/LoginFormProc.aspx?FromLoginFormProc=true" method="post">
 		
 			<!-- Begin Required OnBase Fields -->
@@ -517,5 +475,3 @@
 	</form> 
 	</div>			  
       
-</body>
-</html>
