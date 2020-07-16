a1bcd92880bd398dc6a54738566548af0d829ed2
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-07-16 11:47:19 -0500

    remove form fields

--- intranet/acsforms/deleteForm/landing.php
+++ intranet/acsforms/deleteForm/landing.php
@@ -1,17 +1,4 @@
       <div class="container">
-<!-- this clearly isn't a form??		<form action="https://imageweb.ua.edu/Public/LoginFormProc.aspx?FromLoginFormProc=true" method="post">
--->
-		
-			<!-- Begin Required OnBase Fields -->
-      <!-- I don't think this is necessary??? -caryl
-			<div id="loginformprocparams"> 
-				<input type="hidden" name="LanguageParam" value="en-us" />
-				<input type="hidden" name="OBWeb_FinalTargetPage" value="https://www.google.com"/>
-				<input type="hidden" name="OBDocumentType" value="886" />
-				<input type="hidden" name="OBWeb_Redirect" value="https://www.google.com"/>
-			</div>
-      -->
-			<!-- End Required OnBase Fields -->
 				
 		   <div class="row">
 				<img src="images/librariesk_black.png" alt="UA Libraries Logo" style="width:150px;height:60px"> 
