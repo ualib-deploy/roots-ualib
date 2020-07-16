6452e17620013b488208a64dab38a8bfaa2ab318
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-07-16 11:41:48 -0500

    add external js file, move js out of index

--- intranet/acsforms/voyagerOnlyForm/index.php
+++ intranet/acsforms/voyagerOnlyForm/index.php
@@ -1,44 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-    <!-- Security Authorization Form: Voyager Circulation Only: collapse panels-->     
-    <head>
-    <!-- Required meta tags always come first -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <link rel="stylesheet" type="text/css" href="stylesheet.css">     
-        
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	
-    <!-- Javascript -->
-    
-	<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-    <script type='text/javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <title>Voyager Circulation Only</title>
-		<script>
-			jQuery(document).ready(function($){
-				$('#AssociateDean').change(function(obj){
-			
-					if ($(this).val() == 'Tom Wilson'){
-						$('#AssociateDeanPhone').val('2299');
-						$('#AssociateDeanEmail').val('tcwilson@ua.edu');
-					}
-					if ($(this).val() == 'Millie Jackson'){
-						$('#AssociateDeanPhone').val('5008');
-						$('#AssociateDeanEmail').val('mljackson@ua.edu');
-					}
-					if ($(this).val() == 'Lorraine Madway'){
-						$('#AssociateDeanPhone').val('0513');
-						$('#AssociateDeanEmail').val('lmadway@ua.edu');
-					}
-				});
-			});
-		</script>
-	</head>
-	
-	
-	<body> 
 	<div class="container">
 	<div class="row">
 	<form action="https://imageweb.ua.edu/Public/LoginFormProc.aspx?FromLoginFormProc=true" method="post">
@@ -167,4 +126,3 @@
 
 </div>
 </div>
-</body>
