c57d48a3c86964c22a0e61f7db3bdf50f550da53
caryl wyatt <caryl.e.wyatt@gmail.com>
2020-07-17 15:07:26 -0500

    remove headings, update links

--- intranet/inc/delete-form-landing.php
+++ intranet/inc/delete-form-landing.php
@@ -1,11 +1,7 @@
       <div class="container">
 				
-		   <div class="row">
-				<img src="https://imageweb.ua-net.ua.edu/AppNet/images/librariesk_black.png" alt="UA Libraries Logo" style="width:150px;height:60px"> 
-	       <h1 class="h2">Security Authorization Form</h1>
-		   <h2 class="h3">Deleting Network and Voyager Access </h2> 
-<hr>
-			</div>			  
       
 			<p>Form Submitted Successfully. Please allow 2 - 3 business days for processing.</p>
+    <p><a href="/intranet/delete-form/">Submit another "Delete Network and Voyager Access" form</a></p>
+    <p><a href="/intranet/">Return to UL Intranet</a></p>
 	 </div> 
--- intranet/inc/sec-auth-form-landing.php
+++ intranet/inc/sec-auth-form-landing.php
@@ -6,12 +6,13 @@
     
 	
 		<div class="container">
-			<img src="https://imageweb.ua-net.ua.edu/AppNet/images/librariesk_black.png" alt="UA Libraries Logo" style="width:150px;height:60px">
 				<h1 class="h2">Security Authorization Form</h1>
 				<h2 class="h3">Network & Voyager Access</h2>
 		<hr>
 		
 		<p>Form Submitted Successfully. Please allow 2 - 3 business days for processing.</p>
+    <p><a href="/intranet/security-authorization-form/">Submit another "SAF Network and Voyager Access" form</a></p>
+    <p><a href="/intranet/">Return to UL Intranet</a></p>
 		
 		</div>
 
