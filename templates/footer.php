a2d0681cd92eff05a3729d2f2133caf1954fd4b6
James Michelich <jmichelich@Jamess-MacBook-Pro-Home.local>
2020-10-15 13:27:41 -0500

    footer updates

--- templates/footer.php
+++ templates/footer.php
@@ -3,53 +3,26 @@
         <div class="container">
             <?php //dynamic_sidebar('sidebar-footer'); ?>
             <div class="row">
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                     <h3 class="h4">Quick Links</h3>
                     <ul class="footer-list">
-                        <li><a href="/#/hours">Maps and Contact Information</a></li>
-                        <li><a href="http://ask.lib.ua.edu">Ask A Librarian</a></li>
-                        <li><a href="/using-the-library/accessibility/">Library Accessibility</a></li>
-                        <li><a href="/about/libraries-policies/code-of-conduct/">Code of Conduct</a></li>
                         <li><a href="/about/mission-statement/">Mission Statement</a></li>
-                        <li><a href="/site-map">Site Map</a></li>
                         <li><a href="/ua-libraries-giving/">Support UA Libraries</a></li>
                         <li><a href="/about/employment/">Employment</a></li>
-                        <li><a href="http://alabamacenterforthebook.lib.ua.edu/">Alabama Center for the Book</a></li>
+                        <li><a href="/using-the-library/accessibility/">Library Accessibility</a></li>
+                        <li><a href="/about/libraries-policies/code-of-conduct/">Code of Conduct</a></li>
+                        <li><a href="/site-map">Site Map</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-3">
-                    <h3 class="h4">Libraries</h3>
+                <div class="col-sm-4">
+                    <h3 class="h4">Sponsored Sites</h3>
                     <ul class="footer-list">
-                        <li><a href="/libraries/gorgas/">Amelia Gayle Gorgas Library</a></li>
-                        <li><a href="/libraries/bruno/">Angelo Bruno Business Library</a></li>
-                        <li><a href="/libraries/mclure/">McLure Education Library</a></li>
-                        <li><a href="/libraries/rodgers/">Rodgers Library for Science &amp; Engineering</a></li>
-                        <li><a href="/libraries/hoole/">W.S. Hoole Special Collections Library</a></li>
-                        <li><a href="/libraries/music/">Music Library</a></li>
-                    </ul>
-                    <ul class="footer-list">
-                    </ul>
-                    <ul class="footer-list" collapse="!isCollapsed">
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="http://www.library.law.ua.edu/">Bounds Law Library</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="http://cchs.ua.edu/library/">Health Sciences Library</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="http://alabamamaps.ua.edu/">Map Library & Place Names Research Center</a></li>
-                        <li role="presentation"><a role="menuitem" tabindex="-1" href="https://wgrc.sa.ua.edu/programming-and-resources/frances-s-summersell-library/">Summersell Library</a></li>
-                    </ul>
-                    <button id="show-more-lib-btn" class="btn btn-default btn-xs"  type="button" ng-click="isCollapsed = !isCollapsed">
-                        {{!isCollapsed ? 'Show More' : 'Show Less'}} Libraries...
-                    </button>
-                </div>
-                <div class="col-sm-3" >
-                    <h3 class="h4">Collections</h3>
-                    <ul class="footer-list">
-                        <li><a href="/collections/williams/">A.S. Williams Collection</a></li>
-                        <li><a href="/collections/the-david-walker-lupton-african-american-cookbook-collection/">Lupton Collection</a></li>
-                        <li><a href="/collections/map-collections/">Map Collection</a></li>
-                        <li><a href="/collections/university-archives/">University Archives and Records Management</a></li>
-                        <li><i><a href="/collections/major-collections-summaries/">More Collections...</a></i></li>
+                        <li><a href="https://alabamacenterforthebook.lib.ua.edu/">Alabama Center for the Book</a></li>
+                        <li><a href="https://alabamaliterarymap.lib.ua.edu/">This Goodly Land: Alabama's Literary Landscape</a></li>
+                        <li><a href="/research-tools/alabama-authors/">Alabama Authors</a></li>
                     </ul>
                 </div>
-                <div class="col-xs-12 col-sm-3">
+                <div class="col-xs-12 col-sm-4">
                     <a href="/about-ua-libraries/social-media/">
                         <span class="sr-only">Social Media Page</span>
                         <ul class="nav nav-pills nav-justified social-icons">
